50859d9d2d1b9760770b88a70c71afa2806d4320
chengduoZH <zhaochengduo@163.com>
2018-02-09 17:08:45 +0800

    add

--- paddle/framework/chan_desc.h
+++ paddle/framework/chan_desc.h
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
-
+#include <memory>
 #include <vector>
 #include "glog/logging.h"
 #include "paddle/framework/framework.pb.h"
