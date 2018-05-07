f9d7d6e1fe9a452065526bee9d95a8014204559d
chengduoZH <zhaochengduo@163.com>
2018-05-07 23:38:48 +0800

    refine gru

--- paddle/fluid/operators/math/sequence2batch.cu
+++ paddle/fluid/operators/math/sequence2batch.cu
@@ -19,22 +19,21 @@ namespace paddle {
 namespace operators {
 namespace math {
 
-template <typename T, int BlockDimX, int BlockDimY, int GridDimX>
+template <typename T, bool IsSrcIndex>
 __global__ void CopyMatrixRowsKernel(const T* src, T* dst, const size_t* index,
-                                     int64_t height, int64_t width,
-                                     bool is_src_index) {
+                                     int64_t height, int64_t width) {
   int idx = threadIdx.x;
   int idy = threadIdx.y;
-  int id = blockIdx.x + idy * GridDimX;
+  int id = blockIdx.x + idy * gridDim.x;
   while (id < height) {
-    int src_idx = is_src_index ? index[id] : id;
-    int dst_idx = is_src_index ? id : index[id];
+    int src_idx = IsSrcIndex ? index[id] : id;
+    int dst_idx = IsSrcIndex ? id : index[id];
     const T* src_data = src + src_idx * width;
     T* dst_data = dst + dst_idx * width;
-    for (int i = idx; i < width; i += BlockDimX) {
+    for (int i = idx; i < width; i += blockDim.x) {
       dst_data[i] = src_data[i];
     }
-    id += BlockDimY * GridDimX;
+    id += blockDim.y * gridDim.x;
   }
 }
 
@@ -58,12 +57,27 @@ class CopyMatrixRowsFunctor<platform::CUDADeviceContext, T> {
     auto* src_data = src.data<T>();
     auto* dst_data = dst->data<T>();
 
-    dim3 threads(128, 8);
-    dim3 grid(8, 1);
+    const int thread_rows = 8;
+    const int thread_cols = 128;
+    dim3 threads(thread_cols, thread_rows);
+
+    int max_threads = context.GetMaxPhysicalThreadCount();
+    int max_blocks = std::max(max_threads / (thread_cols * thread_rows), 1);
+
+    int grid_rows = std::min(
+        max_blocks, std::max(static_cast<int>(height / thread_rows), 1));
+
+    dim3 grid(grid_rows, 1);
     auto stream = context.stream();
-    CopyMatrixRowsKernel<T, 128, 8, 8><<<grid, threads, 0, stream>>>(
-        src_data, dst_data, index_lod.CUDAData(context.GetPlace()), height,
-        width, is_src_index);
+    if (is_src_index) {
+      CopyMatrixRowsKernel<T, true><<<grid, threads, 0, stream>>>(
+          src_data, dst_data, index_lod.CUDAData(context.GetPlace()), height,
+          width);
+    } else {
+      CopyMatrixRowsKernel<T, false><<<grid, threads, 0, stream>>>(
+          src_data, dst_data, index_lod.CUDAData(context.GetPlace()), height,
+          width);
+    }
   }
 };
 
