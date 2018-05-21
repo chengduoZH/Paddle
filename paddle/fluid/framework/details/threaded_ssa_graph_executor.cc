a95281118c4bf33e409b453e5873f41bcf98c4a5
chengduoZH <zhaochengduo@163.com>
2018-05-21 13:51:15 +0800

    get timer in threaded_ssa_graph_executor

--- paddle/fluid/framework/details/threaded_ssa_graph_executor.cc
+++ paddle/fluid/framework/details/threaded_ssa_graph_executor.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/timer/Stat.h"
 #include "paddle/fluid/framework/details/threaded_ssa_graph_executor.h"
+#include "paddle/timer/Stat.h"
 
 namespace paddle {
 namespace framework {
@@ -42,34 +42,42 @@ FeedFetchList ThreadedSSAGraphExecutor::Run(
   // together since we currently cannot overlap computation and memcpy streams.
   // Should revisit it if overlapping is available.
   std::unordered_set<OpHandleBase *> delayed_ops;
+  {
+    auto name = "ThreadedSSAGraphExecutor::Run::pending_ops & pending_vars";
+    auto stat = getStat(name);
+    TimerOnce timer(stat.get(), name, 1 * 1LU);
+    // Transform SSAGraph to pending_ops & pending_vars
+    for (auto &var_map : graph_->vars_) {
+      for (auto &name_pair : var_map) {
+        for (auto &version_pair : name_pair.second) {
+          InsertPendingVar(&pending_vars, &ready_vars, version_pair.get());
+        }
+      }
+    }
+    for (auto &var : graph_->dep_vars_) {
+      InsertPendingVar(&pending_vars, &ready_vars, var.get());
+    }
 
-  // Transform SSAGraph to pending_ops & pending_vars
-  for (auto &var_map : graph_->vars_) {
-    for (auto &name_pair : var_map) {
-      for (auto &version_pair : name_pair.second) {
-        InsertPendingVar(&pending_vars, &ready_vars, version_pair.get());
+    for (auto &op : graph_->ops_) {
+      if (op->Inputs().empty()) {  // Special case, Op has no input.
+        ready_ops.insert(op.get());
+      } else {
+        InsertPendingOp(&pending_ops, op.get());
       }
     }
   }
-  for (auto &var : graph_->dep_vars_) {
-    InsertPendingVar(&pending_vars, &ready_vars, var.get());
-  }
-
-  for (auto &op : graph_->ops_) {
-    if (op->Inputs().empty()) {  // Special case, Op has no input.
-      ready_ops.insert(op.get());
-    } else {
-      InsertPendingOp(&pending_ops, op.get());
-    }
-  }
-
   // Step 2. Insert FetchOps
   std::vector<std::unique_ptr<FetchOpHandle>> fetch_ops;
   std::unordered_set<std::unique_ptr<VarHandleBase>> fetch_dependencies;
   FeedFetchList fetch_data(fetch_tensors.size());
 
-  InsertFetchOps(fetch_tensors, &fetch_ops, &fetch_dependencies, &pending_ops,
-                 &pending_vars, &ready_vars, &fetch_data);
+  {
+    auto name = "ThreadedSSAGraphExecutor::Run::Insert FetchOps";
+    auto stat = getStat(name);
+    TimerOnce timer(stat.get(), name, 1 * 1LU);
+    InsertFetchOps(fetch_tensors, &fetch_ops, &fetch_dependencies, &pending_ops,
+                   &pending_vars, &ready_vars, &fetch_data);
+  }
 
   auto run_all_ops = [&](std::unordered_set<OpHandleBase *> &set) {
     for (auto *op : set) {
@@ -79,56 +87,60 @@ FeedFetchList ThreadedSSAGraphExecutor::Run(
     set.clear();
   };
 
-  // Step 3. Execution
-  while (!pending_vars.empty()) {
-    // 1. Run All Ready ops
-    // Keep loop until all vars are ready.
-    //
-    // NOTE: DelayedOps have a lower priority. It will be scheduled after all
-    // ready_ops have been performed.
-    if (ready_ops.empty() && strategy_.allow_op_delay_ && running_ops_ == 0) {
-      run_all_ops(delayed_ops);
-    } else {
-      run_all_ops(ready_ops);
-    }
-
-    // 2. Find ready variable
-    bool timeout;
-    auto cur_ready_vars = ready_vars.PopAll(1, &timeout);
-
-    if (timeout) {
-      if (exception_) {
-        auto exp = *exception_;
-        exception_.reset();
-        throw exp;
+  {
+    auto name = "ThreadedSSAGraphExecutor::Run::Insert FetchOps";
+    auto stat = getStat(name);
+    TimerOnce timer(stat.get(), name, 1 * 1LU);
+    // Step 3. Execution
+    while (!pending_vars.empty()) {
+      // 1. Run All Ready ops
+      // Keep loop until all vars are ready.
+      //
+      // NOTE: DelayedOps have a lower priority. It will be scheduled after all
+      // ready_ops have been performed.
+      if (ready_ops.empty() && strategy_.allow_op_delay_ && running_ops_ == 0) {
+        run_all_ops(delayed_ops);
       } else {
-        continue;
+        run_all_ops(ready_ops);
       }
-    }
-    // 3. Remove the dependency of ready_var.
-    // Find the ready_ops after the ready_var.
-    for (auto ready_var : cur_ready_vars) {
-      pending_vars.erase(ready_var);
-      for (auto *op : ready_var->pending_ops_) {
-        auto &deps = pending_ops[op];
-        --deps;
-        if (deps == 0) {
-          if (op->IsMultiDeviceTransfer() && strategy_.allow_op_delay_) {
-            delayed_ops.insert(op);
-          } else {
-            ready_ops.insert(op);
+
+      // 2. Find ready variable
+      bool timeout;
+      auto cur_ready_vars = ready_vars.PopAll(1, &timeout);
+
+      if (timeout) {
+        if (exception_) {
+          auto exp = *exception_;
+          exception_.reset();
+          throw exp;
+        } else {
+          continue;
+        }
+      }
+      // 3. Remove the dependency of ready_var.
+      // Find the ready_ops after the ready_var.
+      for (auto ready_var : cur_ready_vars) {
+        pending_vars.erase(ready_var);
+        for (auto *op : ready_var->pending_ops_) {
+          auto &deps = pending_ops[op];
+          --deps;
+          if (deps == 0) {
+            if (op->IsMultiDeviceTransfer() && strategy_.allow_op_delay_) {
+              delayed_ops.insert(op);
+            } else {
+              ready_ops.insert(op);
+            }
           }
         }
       }
     }
-  }
-  PADDLE_ENFORCE(ready_ops.empty());
+    PADDLE_ENFORCE(ready_ops.empty());
 
-  // Wait FetchOps.
-  if (!fetch_ops.empty()) {
-    fetch_ops.clear();
+    // Wait FetchOps.
+    if (!fetch_ops.empty()) {
+      fetch_ops.clear();
+    }
   }
-
   return fetch_data;
 }
 
