567057b63a816ca30fc152526093926a0da13e27
chengduozh <zhaochengduo@baidu.com>
2018-11-14 23:18:15 +0800

    add tc cmake

--- cmake/tc.cmake
+++ cmake/tc.cmake
@@ -4,10 +4,8 @@ endif()
 
 set(TC_ROOT "/paddle/tc/install" CACHE PATH "TC ROOT")
 find_path(TC_INCLUDE_DIR Halide.h
-    PATHS ${TC_ROOT} ${TC_ROOT}/include
-    $ENV{TC_ROOT} $ENV{TC_ROOT}/include
-    NO_DEFAULT_PATH
-)
+        PATHS ${TC_ROOT} ${TC_ROOT}/include
+        $ENV{TC_ROOT} $ENV{TC_ROOT}/include)
 
 #get_filename_component(__libpath_hist ${CUDA_CUDART_LIBRARY} PATH)
 
@@ -17,27 +15,23 @@ if(NOT ${CMAKE_SYSTEM_PROCESSOR})
 endif()
 
 list(APPEND TC_CHECK_LIBRARY_DIRS
-    ${TC_ROOT}
-    ${TC_ROOT}/lib
-    $ENV{TC_ROOT}
-    $ENV{TC_ROOT}/lib
-	)
-set(TC_LIB_NAME "")
+        ${TC_ROOT}
+        ${TC_ROOT}/lib
+        $ENV{TC_ROOT}
+        $ENV{TC_ROOT}/lib)
 
-if (LINUX)
-    set(TC_LIB_NAME "llibHalide.so")
-endif(LINUX)
+set(TC_LIB_NAME "libHalide.so")
 
 find_library(TC_LIBRARY NAMES ${TC_LIB_NAME} # libcudnn_static.a
-    PATHS ${TC_CHECK_LIBRARY_DIRS} ${TC_INCLUDE_DIR} ${__libpath_hist}
-          NO_DEFAULT_PATH
-    DOC "Path to cuDNN library.")
+        PATHS ${TC_CHECK_LIBRARY_DIRS} ${TC_INCLUDE_DIR} ${__libpath_hist}
+        NO_DEFAULT_PATH
+        DOC "Path to cuDNN library.")
 
 if(TC_INCLUDE_DIR AND TC_LIBRARY)
     message("TC FOUND")
+    get_filename_component(TC_LIB_PATH ${TC_LIBRARY} DIRECTORY)
     message(${TC_INCLUDE_DIR})
-    message(${TC_LIBRARY})
+    message(${TC_LIB_PATH})
 else()
     message("TC NOT FOUND")
 endif()
-
