fa4fc35d296f3d44ee11722fbbb976b891dcfcdf
chengduozh <zhaochengduo@baidu.com>
2018-11-14 17:45:19 +0800

    temp

---
+++ cmake/tc.cmake
@@ -0,0 +1,41 @@
+if(NOT WITH_GPU)
+    return()
+endif()
+
+set(TC_ROOT "/paddle/tc/install" CACHE PATH "TC ROOT")
+find_path(TC_INCLUDE_DIR Halide.h
+    PATHS ${TC_ROOT} ${TC_ROOT}/include
+    $ENV{TC_ROOT} $ENV{TC_ROOT}/include ${CUDA_TOOLKIT_INCLUDE}
+    NO_DEFAULT_PATH
+)
+
+get_filename_component(__libpath_hist ${CUDA_CUDART_LIBRARY} PATH)
+
+set(TARGET_ARCH "x86_64")
+if(NOT ${CMAKE_SYSTEM_PROCESSOR})
+    set(TARGET_ARCH ${CMAKE_SYSTEM_PROCESSOR})
+endif()
+
+list(APPEND TC_CHECK_LIBRARY_DIRS
+    ${TC_ROOT}
+    ${TC_ROOT}/lib
+    $ENV{TC_ROOT}
+    $ENV{TC_ROOT}/lib
+	)
+set(TC_LIB_NAME "")
+
+if (LINUX)
+    set(TC_LIB_NAME "llibHalide.so")
+endif(LINUX)
+
+find_library(TC_LIBRARY NAMES ${TC_LIB_NAME} # libcudnn_static.a
+    PATHS ${TC_CHECK_LIBRARY_DIRS} ${TC_INCLUDE_DIR} ${__libpath_hist}
+          NO_DEFAULT_PATH
+    DOC "Path to cuDNN library.")
+
+if(TC_INCLUDE_DIR AND TC_LIBRARY)
+    message("TC FOUND")
+else()
+    message("TC NOT FOUND")
+endif()
+
