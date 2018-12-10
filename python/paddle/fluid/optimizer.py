85a4b295579163bea210d8c651a998478ab75eb9
chengduozh <zhaochengduo@baidu.com>
2018-12-10 20:23:34 +0800

    move WeightDecay to regularier test=develop

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -29,8 +29,7 @@ from .framework import program_guard
 from .initializer import Constant
 from .layer_helper import LayerHelper
 from .layers import ops
-from .regularizer import append_regularization_ops
-from .weight_decay import append_weight_decay
+from .regularizer import append_regularization_ops, append_weight_decay
 
 __all__ = [
     'SGD', 'Momentum', 'Adagrad', 'Adam', 'Adamax', 'DecayedAdagrad', 'Ftrl',
@@ -344,6 +343,10 @@ class SGDOptimizer(Optimizer):
         Can be a float value or a Variable with one float value as data element.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -410,6 +413,10 @@ class MomentumOptimizer(Optimizer):
         use_nesterov (bool): enables Nesterov momentum
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -491,6 +498,10 @@ class LarsMomentumOptimizer(Optimizer):
         lars_weight_decay (float): weight decay coefficient for decaying using LARS.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
         
 
@@ -578,6 +589,10 @@ class AdagradOptimizer(Optimizer):
         epsilon (float): a small float value for numerical stability.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -662,6 +677,10 @@ class AdamOptimizer(Optimizer):
         epsilon (float): a small float value for numerical stability.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -815,6 +834,10 @@ class AdamaxOptimizer(Optimizer):
         epsilon (float): a small float value for numerical stability.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -940,6 +963,10 @@ class DecayedAdagradOptimizer(Optimizer):
         epsilon (float): a small float value for numerical stability.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -1024,6 +1051,10 @@ class AdadeltaOptimizer(Optimizer):
         epsilon(float): epsilon in equation
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Examples:
@@ -1160,6 +1191,10 @@ class RMSPropOptimizer(Optimizer):
             computation and memory. Defaults to False.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Raises:
@@ -1297,6 +1332,10 @@ class FtrlOptimizer(Optimizer):
         lr_power (float):
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
 
     Raises:
@@ -1408,6 +1447,10 @@ class ModelAverage(Optimizer):
         max_average_window: The maximum size of average window.
         regularization: A Regularizer, such as
                         fluid.regularizer.L2DecayRegularizer.
+        weight_decay: A WeightDecay, such as fluid.regularizer.WeightDecay. it is used
+                        to update the optimized parameters by the using parameters
+                        before optimization. weigth_decay can receive a float
+                        value as coeff. Default None.
         name: A optional name prefix.
     Examples:
 
--- python/paddle/fluid/regularizer.py
+++ python/paddle/fluid/regularizer.py
@@ -13,11 +13,18 @@
 # limitations under the License.
 
 from __future__ import print_function
-
+import contextlib
+import numpy as np
 from . import framework
 from . import core
 
-__all__ = ['L1Decay', 'L2Decay', 'L1DecayRegularizer', 'L2DecayRegularizer']
+__all__ = [
+    'L1Decay',
+    'L2Decay',
+    'L1DecayRegularizer',
+    'L2DecayRegularizer',
+    'WeightDecay',
+]
 
 
 def append_regularization_ops(parameters_and_grads, regularization=None):
@@ -82,6 +89,31 @@ def append_regularization_ops(parameters_and_grads, regularization=None):
             params_and_grads.append((param, new_grad))
 
     return params_and_grads
+
+
+@contextlib.contextmanager
+def append_weight_decay(param_and_grads, weight_decay=None):
+    """Add decay for the weight.
+
+    Appends weight decay operators in the BlockDesc.
+    This will update the optimized parameters by using the
+    parameters before optimization.
+
+    Args:
+        param_and_grads: A list of (parameters, gradients) pairs
+            that need to be updated.
+        weight_decay(WeightDecayBase): A WeightDecay Objection, such as
+            fluid.weight_decay.WeightDecay.
+
+    Raises:
+        Exception: Unknown weight decay type.
+    """
+
+    if weight_decay is not None:
+        weight_decay(param_and_grads)
+    yield
+    if weight_decay is not None:
+        weight_decay.apply()
 
 
 class WeightDecayRegularizer(object):
@@ -229,6 +261,86 @@ class L1DecayRegularizer(WeightDecayRegularizer):
 
     def __str__(self):
         return "L1Decay, regularization_coeff=%f" % self._regularization_coeff
+
+
+class WeightDecay(object):
+    """
+    WeightDecay is used to update the optimized parameters by using the
+    parameters before optimization.
+
+    Args:
+        coeff (float|Variable): The weight decay coefficient, it can be float
+            or Variable.
+        attempt_decay_param_fun (function|None): If it is not None,
+            only variables that makes attempt_decay_param_fun(variable)==True
+            will be updated. It only works when we want to specify variables.
+            Default: None.
+
+    Examples:
+        .. code-block:: python
+            def name_has_fc(var):
+                res = "fc" in var.name
+                return res
+
+            learning_rate = 0.1
+            optimizer = fluid.optimizer.Adagrad(
+                learning_rate=learning_rate,
+                weight_decay=fluid.weight_decay.WeightDecay(
+                    coeff=learning_rate, attempt_decay_param_fun=name_has_fc))
+    """
+
+    def __init__(self, coeff=0.0, attempt_decay_param_fun=None):
+
+        if not isinstance(coeff, float) and \
+                not isinstance(coeff, framework.Variable):
+            raise TypeError("coeff should be float or Variable.")
+
+        self.scaled_params_ = []
+        self.params_name_ = set()
+        self.attempt_decay_param_fun_ = attempt_decay_param_fun
+        self.coeff_ = coeff
+
+    def __call__(self, params_and_grads):
+        from . import layers
+        if isinstance(self.coeff_, float) and np.isclose(self.coeff_, 0.0):
+            return
+
+        for param, grad in params_and_grads:
+            # If no gradient then we don't need to do anything
+            if grad is None:
+                continue
+            if self.attempt_decay_param_fun_ is not None \
+                    and not self.attempt_decay_param_fun_(param.name):
+                continue
+
+            if isinstance(self.coeff_, float):
+                assert param.dtype is not core.VarDesc.VarType.FP32, \
+                    "the type of coeff(float) and parameter(%s) is not consistent."%(self.coeff_.dtype)
+            else:
+                assert self.coeff_.dtype == param.dtype, \
+                    "the type of coeff(%s) and parameter(%s) is not consistent."%(self.coeff_.dtype, param.dtype)
+
+            with param.block.program._optimized_guard(
+                [param, grad]), framework.name_scope('weight decay'):
+                assert param.name not in self.params_name_
+                self.scaled_params_.append((param, grad, param * self.coeff_))
+                self.params_name_.append(param.name)
+
+    def apply(self):
+        """
+        Update the optimized parameters.
+        """
+        from . import layers
+        if isinstance(self.coeff_, float) and np.isclose(self.coeff_, 0.0):
+            return
+        for p_grad_sgrad in self.scaled_params_:
+            param, grad, scaled_param = p_grad_sgrad
+            with param.block.program._optimized_guard(
+                [param, grad]), framework.name_scope('weight decay'):
+                layers.elementwise_sub(x=param, y=scaled_param, out=param)
+
+    def __str__(self):
+        return " ".join(["Weight Decay, params:", ",".join(self.params_name_)])
 
 
 # We short the class name, since users will use the regulaizer with the package
