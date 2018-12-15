0491ed61ad05459ff8ec94ec17d3fdf9d6d1e324
chengduozh <zhaochengduo@baidu.com>
2018-12-15 18:01:22 +0800

    debug

--- paddle/fluid/platform/temporay_allocator.h
+++ paddle/fluid/platform/temporay_allocator.h
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #pragma once
+#include <condition_variable>  // NOLINT
 #include <deque>
 #include <mutex>  // NOLINT
 #include "paddle/fluid/memory/allocation/allocator.h"
