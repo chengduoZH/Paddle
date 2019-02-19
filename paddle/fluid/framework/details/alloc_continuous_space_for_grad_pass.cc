c05fc5543dcfe7adcd8c32a05d0f9f092e892e17
chengduozh <zhaochengduo@baidu.com>
2019-02-19 13:02:20 +0800

    Polish code test=develop

--- paddle/fluid/framework/details/alloc_continuous_space_for_grad_pass.cc
+++ paddle/fluid/framework/details/alloc_continuous_space_for_grad_pass.cc
@@ -32,17 +32,8 @@ class AllocContinuousSpaceForGradPass : public ir::Pass {
       std::unique_ptr<ir::Graph> graph) const override {
     ir::Graph& result = *graph;
 
-    if (result.Has(kParamsAndGrads)) {
-      VLOG(10) << kParamsAndGrads << " is reset.";
-      result.Erase(kParamsAndGrads);
-    }
-    result.Set(kParamsAndGrads, new ParamsAndGrads);
-
-    if (result.Has(kGroupGradsAndParams)) {
-      VLOG(10) << kGroupGradsAndParams << " is reset.";
-      result.Erase(kGroupGradsAndParams);
-    }
-    result.Set(kGroupGradsAndParams, new GroupGradsAndParams);
+    ResetAttribute<ParamsAndGrads>(kParamsAndGrads, &result);
+    ResetAttribute<GroupGradsAndParams>(kGroupGradsAndParams, &result);
 
     // NOTE: The operator nodes should be in topology order.
     std::vector<ir::Node*> topo_nodes = ir::TopologySortOperations(result);
@@ -63,10 +54,10 @@ class AllocContinuousSpaceForGradPass : public ir::Pass {
 
     // Note: Sort the parameters and gradient variables according
     // to parameters' name to make variables' name correspond correctly.
+    SortParamsAndGrads(vars, &params_grads);
     auto& group_params_grads =
         result.Get<GroupGradsAndParams>(kGroupGradsAndParams);
-    ResortParamsAndGrads(vars, &params_grads, &group_params_grads);
-
+    RecordGroupGradsAndParams(params_grads, &group_params_grads);
     // Set Gradients as Persistable to prevent this var becoming reusable.
     auto dtype = static_cast<proto::VarType::Type>(0);
     for (auto& p_g : params_grads) {
@@ -119,17 +110,30 @@ class AllocContinuousSpaceForGradPass : public ir::Pass {
     return std::move(graph);
   }
 
-  void ResortParamsAndGrads(
-      const std::unordered_map<std::string, ir::Node*>& var_nodes,
-      ParamsAndGrads* params_grads,
-      GroupGradsAndParams* group_params_grads) const {
-    // TODO(zcd): The sort should be removed.
-    std::sort(params_grads->begin(), params_grads->end(),
-              [](const std::pair<std::string, std::string>& a,
-                 const std::pair<std::string, std::string>& b) -> bool {
-                return a.first < b.first;
-              });
+  template <typename AttrType>
+  void ResetAttribute(const std::string& attr_name, ir::Graph* graph) const {
+    if (graph->Has(attr_name)) {
+      VLOG(10) << attr_name << " is reset.";
+      graph->Erase(attr_name);
+    }
+    graph->Set(attr_name, new AttrType);
+  }
 
+  void SortParamsAndGrads(
+      const std::unordered_map<std::string, ir::Node*>& var_nodes,
+      ParamsAndGrads* params_grads) const {
+    // TODO(zcd): sort the params_grads according to var's dim
+    // TODO(zcd): The sort should be removed.
+    //    std::sort(params_grads->begin(), params_grads->end(),
+    //              [](const std::pair<std::string, std::string>& a,
+    //                 const std::pair<std::string, std::string>& b) -> bool {
+    //                return a.first > b.first;
+    //              });
+  }
+
+  void RecordGroupGradsAndParams(
+      const ParamsAndGrads& params_grads,
+      GroupGradsAndParams* group_params_grads) const {
     // group_size
     const size_t group_size = 3;
     size_t groups = (params_grads.size() + group_size - 1) / group_size;
@@ -142,11 +146,11 @@ class AllocContinuousSpaceForGradPass : public ir::Pass {
       group_p_g.reserve(group_size);
       VLOG(10) << "Group:" << i;
       std::stringstream out;
-      while (j < params_grads->size()) {
+      while (j < params_grads.size()) {
         group_p_g.emplace_back(
-            std::make_pair(params_grads->at(j).second /*grad*/,
-                           params_grads->at(j).first /*param*/));
-        out << params_grads->at(j).second << "[" << params_grads->at(j).first
+            std::make_pair(params_grads.at(j).second /*grad*/,
+                           params_grads.at(j).first /*param*/));
+        out << params_grads.at(j).second << "[" << params_grads.at(j).first
             << "]  ";
         ++j;
         if (j % group_size == 0) break;
