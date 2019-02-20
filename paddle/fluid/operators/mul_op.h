c143aef0b745987a1327b454ca977401d5a9916d
chengduozh <zhaochengduo@baidu.com>
2019-02-20 12:22:08 +0800

    debug  mul

--- paddle/fluid/operators/mul_op.h
+++ paddle/fluid/operators/mul_op.h
@@ -28,10 +28,11 @@ class MulKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
     const Tensor* x = context.Input<Tensor>("X");
-    const Tensor* y = context.Input<Tensor>("Y");
+    const Tensor* origin_y = context.Input<Tensor>("Y");
     Tensor tmp_y;
-    framework::TensorCopy(*y, context.GetPlace(), context.device_context(),
-                          &tmp_y);
+    framework::TensorCopy(*origin_y, context.GetPlace(),
+                          context.device_context(), &tmp_y);
+    const Tensor* y = &tmp_y;
     Tensor* z = context.Output<Tensor>("Out");
     const Tensor x_matrix =
         x->dims().size() > 2
