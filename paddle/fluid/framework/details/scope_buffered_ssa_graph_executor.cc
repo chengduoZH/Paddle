b4a957c2885208e7f3d6e8642cb7bf8072402816
chengduozh <zhaochengduo@baidu.com>
2019-03-16 20:02:44 +0800

    remove tmp

--- paddle/fluid/framework/details/scope_buffered_ssa_graph_executor.cc
+++ paddle/fluid/framework/details/scope_buffered_ssa_graph_executor.cc
@@ -39,7 +39,7 @@ ScopeBufferedSSAGraphExecutor::ScopeBufferedSSAGraphExecutor(
 FeedFetchList ScopeBufferedSSAGraphExecutor::Run(
     const std::vector<std::string> &fetch_tensors) {
   if (drop_scope_counter_ == 0) {
-    static int flag = -1;
+    std::atomic<int> flag{-1};
     // Create local scopes.
     for (auto it = local_scopes_.rbegin(); it != local_scopes_.rend(); ++it) {
       auto &scope = *it;
@@ -69,14 +69,14 @@ FeedFetchList ScopeBufferedSSAGraphExecutor::Run(
           InitializeVariable(scope->Var(info.name_), info.type_);
         } else {
           ++i;
-          if (flag == -1 && i >= begin && i < end) {
+          if (flag < 4 && i >= begin && i < end) {
             InitializeVariable(scope->Var(info.name_), info.type_);
           } else {
             InitializeVariable(local_scope.Var(info.name_), info.type_);
           }
         }
       }
-      flag = 0;
+      flag++;
       if (VLOG_IS_ON(10)) {
         {
           auto vars = local_scope.LocalVarNames();
