c1cf5a471b9772f93336010e681606da9552f435
chengduozh <zhaochengduo@baidu.com>
2019-03-16 18:42:38 +0800

    keep tmp

--- paddle/fluid/framework/details/scope_buffered_ssa_graph_executor.cc
+++ paddle/fluid/framework/details/scope_buffered_ssa_graph_executor.cc
@@ -43,15 +43,15 @@ FeedFetchList ScopeBufferedSSAGraphExecutor::Run(
           &local_scope;
 
       std::stringstream out2;
-      int64_t begin = 0, end = var_infos_.size() / 4 * 2;
-      int64_t i = -1;
+      //      int64_t begin = 0, end = var_infos_.size() / 4 * 2;
+      //      int64_t i = -1;
       for (auto &info : var_infos_) {
-        ++i;
+        //        ++i;
         if (scope->FindVar(info.name_) != nullptr) {
           continue;
         }
-
-        if (info.persistable_ || (i >= begin && i < end)) {  //
+        auto pos = info.name_.find("tmp");
+        if (info.persistable_ || pos != std::string::npos) {  //
           //        Persistable
           //        if (info.persistable_) {
           if (VLOG_IS_ON(10)) {
@@ -102,35 +102,35 @@ FeedFetchList ScopeBufferedSSAGraphExecutor::Run(
     local_scope->DropKids();
   }
 
-  if (drop_scope_counter_ == strategy_.num_iteration_per_drop_scope_) {
-    if (!stream_end) {
-      WaitComputationalStreams();
-    }
-
-    for (auto &scope : local_scopes_) {
-      auto &local_scope =
-          *scope->Var(details::kLocalExecScopeName)->GetMutable<Scope *>();
-      {
-        std::stringstream out;
-        std::stringstream out2;
-        std::unordered_set<std::string> vars2;
-        auto vars = local_scope->LocalVarNames();
-        for (auto var : vars) {
-          out << var << ", ";
-          if (create_vars_[scope].count(var) == 0) {
-            out2 << var << ", ";
-            vars2.insert(var);
-          }
-        }
-        VLOG(10) << "delete local scope of " << scope << ", " << vars.size()
-                 << ", " << out.str() << "";
-        VLOG(10) << "not in local scope " << scope << ", " << vars2.size()
-                 << ", " << out2.str() << "";
-      }
-      scope->DeleteScope(local_scope);
-    }
-    drop_scope_counter_ = 0;
+  //  if (drop_scope_counter_ == strategy_.num_iteration_per_drop_scope_) {
+  if (!stream_end) {
+    WaitComputationalStreams();
   }
+
+  for (auto &scope : local_scopes_) {
+    auto &local_scope =
+        *scope->Var(details::kLocalExecScopeName)->GetMutable<Scope *>();
+    {
+      std::stringstream out;
+      std::stringstream out2;
+      std::unordered_set<std::string> vars2;
+      auto vars = local_scope->LocalVarNames();
+      for (auto var : vars) {
+        out << var << ", ";
+        if (create_vars_[scope].count(var) == 0) {
+          out2 << var << ", ";
+          vars2.insert(var);
+        }
+      }
+      VLOG(10) << "delete local scope of " << scope << ", " << vars.size()
+               << ", " << out.str() << "";
+      VLOG(10) << "not in local scope " << scope << ", " << vars2.size() << ", "
+               << out2.str() << "";
+    }
+    scope->DeleteScope(local_scope);
+  }
+  drop_scope_counter_ = 0;
+  //  }
 
   if (eptr) {
     std::rethrow_exception(eptr);
