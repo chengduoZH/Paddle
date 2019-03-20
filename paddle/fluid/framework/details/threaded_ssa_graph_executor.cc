fb59fe0598602f34a621cd8706a88b8963aa1944
chengduozh <zhaochengduo@baidu.com>
2019-03-20 20:25:45 +0800

    Debug

--- paddle/fluid/framework/details/threaded_ssa_graph_executor.cc
+++ paddle/fluid/framework/details/threaded_ssa_graph_executor.cc
@@ -61,7 +61,6 @@ FeedFetchList ThreadedSSAGraphExecutor::Run(
     }
     set.clear();
   };
-  auto run_all_op = [&](OpHandleBase *op) { RunOp(ready_vars, op); };
   // Clean run context
   run_op_futures_.clear();
   exception_holder_.Clear();
@@ -95,7 +94,7 @@ FeedFetchList ThreadedSSAGraphExecutor::Run(
         auto &deps = pending_ops[op];
         --deps;
         if (deps == 0) {
-          run_all_op(op);
+          ready_ops.insert(op);
         }
       }
     }
